5b865f343b8481da5f478073fe8b1ed708bad2fd
jason2020 <jtay20@punahou.edu>
2022-11-06 00:29:55 -0700

    Attempt to fix CI issues again

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { fontAssets } from 'theme/fonts'
 import { Amplify } from 'aws-amplify'
 import Navigator from './navigator'
 
+// eslint-disable-next-line import/no-unresolved
 import awsconfig from './aws-exports'
 
 Amplify.configure(awsconfig)
