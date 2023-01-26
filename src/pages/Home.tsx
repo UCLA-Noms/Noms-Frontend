8ac750248b18680050b192c502cfbc87a87c8279
Justin Sheu <justinsheu3341@gmail.com>
2023-01-25 21:51:10 -0800

    added basic search bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
-import { StyleSheet, StatusBar, SafeAreaView, Image } from "react-native"
+import { StyleSheet, StatusBar, SafeAreaView, Image, TextInput, View, Button } from "react-native"
 import { ScrollView } from "react-native-gesture-handler"
 import { vw } from "react-native-expo-viewport-units"
 import RestaurantCard from "../components/RestaurantCard"
 import { images } from "../theme"
+import { SearchBar } from '@rneui/themed'
 
 const styles = StyleSheet.create({
   root: {
@@ -20,51 +21,64 @@ const styles = StyleSheet.create({
   },
 })
 
-const Home = ({ navigation }) => (
-  <SafeAreaView style={styles.root}>
-    <StatusBar barStyle="light-content" />
-    <ScrollView style={{ width: "100%" }}>
-      <Image style={{ width: vw(100), height: vw(30) }} source={images.bruh} />
-      <RestaurantCard
-        restaurantName="Drey"
-        closingTime={new Date()}
-        rating={2.3}
-        price={4}
-        distance={1.5}
-        liked={false}
-        navigation={navigation}
-        image="rListing4"
-      />
-      <RestaurantCard
-        restaurantName="Egg Tuck"
-        closingTime={new Date()}
-        rating={2.3}
-        price={4}
-        distance={1.5}
-        liked={false}
-        image="rListing1"
-      />
-      <RestaurantCard
-        restaurantName="Le Pain Quotidien"
-        closingTime={new Date()}
-        rating={2.3}
-        price={4}
-        distance={1.5}
-        liked={false}
-        image="rListing2"
-      />
-      <RestaurantCard
-        restaurantName="Restaurant 4"
-        closingTime={new Date()}
-        rating={2.3}
-        price={4}
-        distance={1.5}
-        liked={false}
-        image="rListing3"
-      />
-    </ScrollView>
-  </SafeAreaView>
-)
+const Home = ({ navigation }) => {
+  const [searchText, setSearch] = React.useState("")
+  return (
+    <SafeAreaView style={styles.root}>
+      <StatusBar barStyle="light-content" />
+      <ScrollView style={{ width: "100%" }}>
+        {/* <Image style={{ width: vw(100), height: vw(30) }} source={images.bruh} /> */}
+        <View
+          style={{ borderRadius: 10, padding: 10, margin: 20, borderWidth: 1, flexDirection: "row", justifyContent: "space-between" }}>
+          <TextInput
+            style={{ flexGrow: 1, width: "80%", height: "100%" }}
+            onChangeText={(text) => setSearch(text)}
+            onSubmitEditing={() => console.log("searching for " + searchText)}
+            value={searchText}
+          />
+          <Button title={'test'} />
+        </View>
+        <RestaurantCard
+          restaurantName="Drey"
+          closingTime={new Date()}
+          rating={2.3}
+          price={4}
+          distance={1.5}
+          liked={false}
+          navigation={navigation}
+          image="rListing4"
+        />
+        <RestaurantCard
+          restaurantName="Egg Tuck"
+          closingTime={new Date()}
+          rating={2.3}
+          price={4}
+          distance={1.5}
+          liked={false}
+          image="rListing1"
+        />
+        <RestaurantCard
+          restaurantName="Le Pain Quotidien"
+          closingTime={new Date()}
+          rating={2.3}
+          price={4}
+          distance={1.5}
+          liked={false}
+          image="rListing2"
+        />
+        <RestaurantCard
+          restaurantName="Restaurant 4"
+          closingTime={new Date()}
+          rating={2.3}
+          price={4}
+          distance={1.5}
+          liked={false}
+          image="rListing3"
+        />
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
 
 Home.propTypes = {
   navigation: PropTypes.shape({
